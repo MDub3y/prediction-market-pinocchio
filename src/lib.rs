b89ac12c1ef1589752673b33c2c5ebacdbbc618b
Awesxme <manasvi.d007@gmail.com>
2026-05-29 18:13:52 +0000

    added basic entrypoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,29 @@
+#![no_std]
+
+use pinocchio::{
+    AccountView, Address, ProgramResult, error::ProgramError, no_allocator, nostd_panic_handler,
+    program_entrypoint,
+};
+
 pub mod state;
+
+program_entrypoint!(process_instruction);
+nostd_panic_handler!();
+no_allocator!();
+
+pub fn process_instruction(
+    program_id: &Address,
+    accounts: &mut [AccountView],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    let (discriminator, rest) = instruction_data
+        .split_first()
+        .ok_or(ProgramError::InvalidInstructionData)?;
+
+    match discriminator {
+        0 => Ok(()),
+        _ => Err(ProgramError::InvalidInstructionData),
+    };
+
+    Ok(())
+}
