7004a895d1117459a0489625cc5d66a66a320010
Awesxme <manasvi.d007@gmail.com>
2026-06-13 18:58:57 +0000

    added split ix

---
+++ src/instructions/place_order/split.rs
@@ -0,0 +1,29 @@
+use crate::state::{MarketUserState, PlaceOrderArgs, PlatformUserState};
+use pinocchio::{AccountView, ProgramResult, error::ProgramError};
+
+pub fn execute_split_operation(
+    accounts: &mut [AccountView],
+    args: &PlaceOrderArgs,
+) -> ProgramResult {
+    let [_, _, platform_user_state, market_user_state, ..] = accounts else {
+        return Err(ProgramError::NotEnoughAccountKeys);
+    };
+
+    unsafe {
+        let mut p_data = platform_user_state.borrow_unchecked_mut();
+        let platform_state = &mut *(p_data.as_mut_ptr() as *mut PlatformUserState);
+        if platform_state.collateral_available < args.quantity {
+            return Err(ProgramError::InsufficientFunds);
+        }
+        platform_state.collateral_available -= args.quantity;
+    }
+
+    unsafe {
+        let mut m_data = market_user_state.borrow_unchecked_mut();
+        let market_user = &mut *(m_data.as_mut_ptr() as *mut MarketUserState);
+        market_user.ot_a_balance += args.quantity;
+        market_user.ot_b_balance += args.quantity;
+    }
+
+    Ok(())
+}
